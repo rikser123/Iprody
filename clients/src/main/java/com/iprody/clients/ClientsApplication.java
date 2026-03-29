ed087db3352fb0489f2153f65fabba70a48751df
Leonid Shikhalkin <rikser123@yandex.ru>
2026-03-29 20:48:33 +0300

    infrastucture: docker-compose, dockerfiles, keycloak

--- clients/src/main/java/com/iprody/clients/ClientsApplication.java
+++ clients/src/main/java/com/iprody/clients/ClientsApplication.java
@@ -3,6 +3,7 @@ package com.iprody.clients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("CheckStyle")
 @SpringBootApplication
 public class ClientsApplication {
 
